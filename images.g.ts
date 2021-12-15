f337f5ddd6510bcaf39771564c15bcc86f5ee76f
sbannonCN <87032965+sbannonCN@users.noreply.github.com>
2021-12-15 12:58:08 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . f f f . . . . f f f . . . 
+. . . f 5 d f . . f d 5 f . . . 
+. . . f 4 5 d f 3 d 5 4 f . . . 
+. f f f f f f f f f f f f f f . 
+f 9 9 9 9 9 f 3 3 f 6 9 9 9 9 f 
+f 9 6 6 6 6 f 3 d f 6 6 6 6 6 f 
+f 9 6 6 6 6 f 5 d f 6 6 6 6 6 f 
+. f f f f f f 4 3 f f f f f f . 
+. f 8 8 8 8 f 5 d f 8 8 8 6 f . 
+. f 6 6 6 6 f 5 d f 6 6 6 6 f . 
+. f 6 6 6 6 f 5 d f 6 6 6 6 f . 
+. f 6 6 6 6 f 5 d f 6 6 6 6 f . 
+. f 6 6 6 6 f 5 d f 6 6 6 6 f . 
+. f 6 6 6 6 f 3 d f 6 6 6 6 f . 
+. f 6 6 6 8 f 3 3 f 8 8 6 6 f . 
+. . f f f f f f f f f f f f . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
